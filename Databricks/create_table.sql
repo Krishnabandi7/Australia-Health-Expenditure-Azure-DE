ee3151f2dcf6d83200089bd1fc7d57112d9ae23a
bandi4-s <gopalbandi04@gmail.com>
2026-05-23 23:38:39 -0500

    merge-

--- Databricks/create_table.sql
+++ Databricks/create_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE customer (
     customer_id INT PRIMARY KEY,
-    first_name VARCHAR(50),
+    first_name VARCHAR(100),
     last_name VARCHAR(50),
+    email VARCHAR(100),
     created_at TIMESTAMP
 );
