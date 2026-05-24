0facb7c077dd1a9c173448feaaa432d5f527cb03
bandi4-s <gopalbandi04@gmail.com>
2026-05-23 23:51:09 -0500

    add_files

--- Databricks/create_table.sql
+++ Databricks/create_table.sql
@@ -1,6 +1,10 @@
 CREATE TABLE customer (
     customer_id INT PRIMARY KEY,
     first_name VARCHAR(100),
+    first_name VARCHAR(100),
     last_name VARCHAR(50),
+    email VARCHAR(100),
+    email VARCHAR(100),
+    hii varchar(100)
     created_at TIMESTAMP
 );
